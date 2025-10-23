c2ad21ab65aa3646594d1699a2d64b1b90847019
Al Fajri <alfafajri81@gmail.com>
2025-10-23 15:12:25 +0700

    Progress 82% dengan mengkesampingkan pemahaman penuh

--- form/index.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const inputMaxLengthOnLoad = document.getElementById("inputNama").maxLength;
-  document.getElementById("sisaKarakter").innerText = inputMaxLengthOnLoad;
-});
